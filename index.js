ab1cd7ff9604994519cb0ad71ec9e14b1ad675bb
Gazy Mahomar <gmahomarf@gmail.com>
2015-09-15 01:16:00 -0600

    Committing before I decide to scrap it all!

--- index.js
+++ index.js
@@ -1,191 +1,69 @@
 var request = require("request");
 var uuid = require("uuid");
 
+var utils = require("./utils");
+
 var baseUrl = "http://internal-comfybuffalo-1-dev.apphb.com";
-var englishWords = [
-    "drool",
-    "cats",
-    "clean",
-    "code",
-    "dogs",
-    "materials",
-    "needed",
-    "this",
-    "is",
-    "hard",
-    "what",
-    "are",
-    "you",
-    "smoking",
-    "shot",
-    "gun",
-    "down",
-    "river",
-    "super",
-    "man",
-    "rule",
-    "acklen",
-    "developers",
-    "are",
-    "amazing"
-];
 
 var lists = [];
 
-var isLower = function(c) {
-    return c >= "a" && c <= "z";
-};
+var limit = process.argv[2] && parseInt(process.argv[2]) || 20;
 
-var isUpper = function(c) {
-    return c >= "A" && c <= "Z";
-};
-
-var isConsonant = function(c) {
-    return "bcdfghjklmnpqrstvwxyz".indexOf(c.toLowerCase()) !== -1;
-};
-
-var isVowel = function(c) {
-    return "aeiou".indexOf(c.toLowerCase()) !== -1;
-};
-
-var Fibo = function () {
-    var x = 0;
-    var y = 1;
-
-    this.next = function() {
-        y += x;
-        x = y - x;
-        return x;
-    };
-};
-
-var splitEnglishWords = function(words) {
-    var newWords = [];
-    words.forEach(function(w) {
-        var s = 0;
-        for (var i = 0; i <= w.length; i++) {
-            var slice = w.slice(s,i);
-            if (englishWords.indexOf(slice) !== -1) {
-                newWords.push(slice);
-                s = i;
+for (var times = 0; times < limit; times++) {
+    var uid = uuid.v4();
+    (function(u) {
+        request({
+            url: baseUrl + "/values/" + u,
+            headers: {
+                Accept: "application/json"
             }
-        }
-        if (s < w.length) {
-            newWords.push(w.slice(s));
-        }
-    });
-    return newWords;
-};
+        }, function(err, response, body) {
+            console.log("GET %s", response.request.uri.href)
+            var r = JSON.parse(body);
+            r.uuid = u;
+            lists.push(r);
 
-var shiftVowels = function(words) {
-    var r = [];
-    words.forEach(function(w) {
-        w = w.split("");
-        for (var i = 0; i < w.length; i++) {
-            if (isVowel(w[i])) {
-                if (i === w.length - 1) {
-                    w.unshift(w.pop());
-                } else {
-                    var t = w[i];
-                    w[i] = w[i + 1];
-                    w[i + 1] = t;
+            var base64Encoded = utils.getBase64Data(r);
+
+            console.log(baseUrl + "/encoded/" + u);
+            request({
+                url: baseUrl + "/encoded/" + u,
+                headers: {
+                    Accept: "application/json"
                 }
-                ++i;
-            }
-        }
-        r.push(w.join(""));
-    });
-    return r;
-};
+            }, function(err, response, body) {
+                console.log(base64Encoded);
+                console.dir(body);
+                // process.exit();
+            });
 
-var alternateConsonantCase = function(words) {
-    var toUpper = null;
-    var r = [];
-    words.forEach(function(w) {
-        w = w.split("");
-        for (var i = 0; i < w.length; i++) {
-            if (isConsonant(w[i])) {
-                if (toUpper === null) {
-                    toUpper = isLower(w[i]);
-                    continue;
-                }
-                w[i] = toUpper && w[i].toUpperCase() || w[i].toLowerCase();
-                toUpper = !toUpper;
-            }
-        }
-        r.push(w.join(""));
-    });
-
-    return r;
-};
-
-var replaceVowelsWithFibonacci = function(words, f) {
-    var r = [];
-    var fibo = new Fibo();
-    var c;
-    do {
-        c = fibo.next();
-    } while (c !== f);
-    words.forEach(function(w) {
-        w = w.split("");
-        for (var i = 0; i < w.length; i++) {
-            if (isVowel(w[i])) {
-                w[i] = c;
-                c = fibo.next();
-            }
-        }
-        r.push(w.join(""));
-    });
-
-    return r;
-};
-
-var concatWithAsciiDelim = function(words) {
-    var r = "";
-    words.forEach(function(w, i) {
-        var c;
-        if (i === 0) {
-            c = words[words.length - 1].charCodeAt(0);
-        } else {
-            c = words[i - 1].charCodeAt(0);
-        }
-        r += w + c.toString();
-    });
-    return r;
-};
-
-var postTemplate = {
-  "encodedValue": "SoMeThInG-eNcOdEd",
-  "emailAddress": "gmahomarf@gmail.com",
-  "name": "Gazy Mahomar",
-  "webhookUrl": "http://your-server.com",
-  "repoUrl": "https://github.com/gmahomarf/aa-dev-challenge"
-};
-
-var u = uuid.v4();
-request(baseUrl + "/values/" + u, function(err, response, body) {
-    var r = body;
-    r.uuid = u;
-    lists.push(r);
-    var newWords = splitEnglishWords(r.words);
-    var sortedWords = newWords.sort(function(a, b) {
-        var la = a.toLowerCase();
-        var lb = b.toLowerCase();
-
-        return a < b ? -1 : (a > b ? 1 : 0);
-    });
-    var shiftedVowelWords = shiftVowels(sortedWords);
-    var alternatedConsonantWords = alternateConsonantCase(shiftedVowelWords);
-    var fiboReplaced = replaceVowelsWithFibonacci(alternatedConsonantWords, r.startingFibonacciNumber);
-    var resorted = fiboReplaced.sort(function(a, b) {
-        var la = a.toLowerCase();
-        var lb = b.toLowerCase();
-
-        return a < b ? 1 : (a > b ? -1 : 0);
-    });
-
-    var concated = concatWithAsciiDelim(resorted);
-
-    var base64Encoded = new Buffer(concated, "utf-8").toString("base64");
-});
-
+            (function(id, b64) {
+                var postData = utils.postTemplate(b64);
+                console.log("POST %s", baseUrl + "/values/" + id);
+                //console.log(JSON.stringify(postData, null, 4));
+                request({
+                    method: "POST",
+                    uri: baseUrl + "/values/" + id,
+                    headers: {
+                        Accept: "application/json",
+                        "Content-Type": "application/json"
+                    },
+                    json: true,
+                    body: postData
+                }, function(err, response, body) {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+                    console.log(response.request.payload);
+                    if (response.statusCode === 200) {
+                        console.dir(body);
+                    } else {
+                        console.log("Status: %s", response.statusCode);
+                        console.log(body);
+                    }
+                });
+            })(u, base64Encoded);
+        });
+    })(uid);
+}
---
+++ utils.js
@@ -0,0 +1,198 @@
+var utils = module.exports = {};
+
+var englishWords = [
+    "drool",
+    "cats",
+    "clean",
+    "code",
+    "dogs",
+    "materials",
+    "needed",
+    "this",
+    "is",
+    "hard",
+    "what",
+    "are",
+    "you",
+    "smoking",
+    "shot",
+    "gun",
+    "down",
+    "river",
+    "super",
+    "man",
+    "rule",
+    "acklen",
+    "developers",
+    "are",
+    "amazing"
+];
+
+utils.isLower = function(c) {
+    return c >= "a" && c <= "z";
+};
+
+utils.isUpper = function(c) {
+    return c >= "A" && c <= "Z";
+};
+
+utils.isConsonant = function(c) {
+    return "bcdfghjklmnpqrstvwxz".indexOf(c.toLowerCase()) !== -1;
+};
+
+utils.isVowel = function(c) {
+    return "aeiouy".indexOf(c.toLowerCase()) !== -1;
+};
+
+var Fibo = function() {
+    var x = 0;
+    var y = 1;
+
+    this.next = function() {
+        y += x;
+        x = y - x;
+        return x;
+    };
+};
+
+utils.splitEnglishWords = function(words) {
+    var lowerWords = words.map(function(w) {
+        return w.toLowerCase();
+    });
+    var newWords = [];
+    lowerWords.forEach(function(w, n) {
+        var s = 0;
+        for (var i = 0; i <= w.length; i++) {
+            var slice = w.slice(s, i);
+            if (englishWords.indexOf(slice) !== -1) {
+                newWords.push(words[n].slice(s,i));
+                s = i;
+            }
+        }
+        if (s < w.length) {
+            newWords.push(words[n].slice(s));
+        }
+    });
+    process.env.AADEBUG === "y" && console.log("%s returns %s", "splitEnglishWords", newWords);
+    return newWords;
+};
+
+utils.shiftVowels = function(words) {
+    var r = [];
+    words.forEach(function(w) {
+        w = w.split("");
+        for (var i = 0; i < w.length; i++) {
+            if (utils.isVowel(w[i])) {
+                if (i === w.length - 1) {
+                    w.unshift(w.pop());
+                } else {
+                    var t = w[i];
+                    w[i] = w[i + 1];
+                    w[i + 1] = t;
+                }
+                ++i;
+            }
+        }
+        r.push(w.join(""));
+    });
+    process.env.AADEBUG === "y" && console.log("%s returns %s", "shiftVowels", r);
+    return r;
+};
+
+utils.alternateConsonantCase = function(words) {
+    var toUpper = null;
+    var r = [];
+    words.forEach(function(w) {
+        w = w.split("");
+        for (var i = 0; i < w.length; i++) {
+            if (utils.isConsonant(w[i])) {
+                if (toUpper === null) {
+                    toUpper = utils.isLower(w[i]);
+                    continue;
+                }
+                w[i] = toUpper && w[i].toUpperCase() || w[i].toLowerCase();
+                toUpper = !toUpper;
+            }
+        }
+        r.push(w.join(""));
+    });
+
+    process.env.AADEBUG === "y" && console.log("%s returns %s", "alternateConsonantCase", r);
+    return r;
+};
+
+utils.replaceVowelsWithFibonacci = function(words, f) {
+    var r = [];
+    var fibo = new Fibo();
+    var c;
+    do {
+        c = fibo.next();
+    } while (c !== f);
+    words.forEach(function(w) {
+        w = w.split("");
+        for (var i = 0; i < w.length; i++) {
+            if (utils.isVowel(w[i])) {
+                w[i] = c;
+                c = fibo.next();
+            }
+        }
+        r.push(w.join(""));
+    });
+
+    process.env.AADEBUG === "y" && console.log("%s returns %s", "replaceVowelsWithFibonacci", r);
+    return r;
+};
+
+utils.concatWithAsciiDelim = function(words) {
+    var r = "";
+    words.forEach(function(w, i) {
+        var c;
+        if (i === 0) {
+            c = words[words.length - 1].charCodeAt(0);
+        } else {
+            c = words[i - 1].charCodeAt(0);
+        }
+        r += w + c.toString();
+    });
+    process.env.AADEBUG === "y" && console.log("%s returns %s", "concatWithAsciiDelim", r);
+    return r;
+};
+
+utils.postTemplate = function(value) {
+    return {
+        "encodedValue": value,
+        "emailAddress": "gmahomarf@gmail.com",
+        "name": "Gazy Mahomar",
+        "webhookUrl": "https://aa-dev-challenge.herokuapp.com/webhook",
+        "repoUrl": "https://github.com/gmahomarf/aa-dev-challenge"
+    };
+};
+
+utils.getBase64Data = function(r) {
+    process.env.AADEBUG === "y" && console.log(r);
+    var newWords = utils.splitEnglishWords(r.words);
+    var sortedWords = newWords.sort(function(a, b) {
+        var la = a.toLowerCase();
+        var lb = b.toLowerCase();
+
+        return la < lb ? -1 : (la > lb ? 1 : 0);
+    });
+
+    process.env.AADEBUG === "y" && console.log("%s returns %s", "sortedWords", sortedWords);
+    var shiftedVowelWords = utils.shiftVowels(sortedWords);
+    var alternatedConsonantWords = utils.alternateConsonantCase(shiftedVowelWords);
+    var fiboReplaced = utils.replaceVowelsWithFibonacci(alternatedConsonantWords, r.startingFibonacciNumber);
+    var resorted = fiboReplaced.sort(function(a, b) {
+        var la = a.toLowerCase();
+        var lb = b.toLowerCase();
+
+        return la < lb ? 1 : (la > lb ? -1 : 0);
+    });
+    process.env.AADEBUG === "y" && console.log("%s returns %s", "resorted" , resorted);
+
+    var concated = utils.concatWithAsciiDelim(resorted);
+
+    var base64Encoded = new Buffer(concated, "utf-8").toString("base64");
+
+    return base64Encoded;
+};
